1be55810370f364b2a9dcbbe159f76b379c1780d
Carlon Mc Kay <mckay.carlon@outlook.com>
2026-01-30 16:10:36 +0000

    Updated database.py

--- WebApps/GeekFormAI/backend/app/database.py
+++ WebApps/GeekFormAI/backend/app/database.py
@@ -1,12 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://geekform:geekform@localhost:5432/geekformdb"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://geekform:geekform@db:5432/geekformdb"
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def init_db():
-    from .models import Document  # import your models here
+    from .models import Document
     Base.metadata.create_all(bind=engine)
